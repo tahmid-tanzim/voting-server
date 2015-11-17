99adf81f8db5f7d192eb06b767986f300de7dafb
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-11-17 20:10:06 +0600

    Fixed issued in Immutable Specs

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,26 +1,30 @@
 import {expect} from 'chai';
 import {List, Map} from 'immutable';
 
-describe('immutability', () = > {
+describe('immutability', () => {
 
-    describe('a number', () = > {
+    describe('a number', () => {
+
         function increment(currentState) {
             return currentState + 1;
         }
 
-    it('is immutable', () = > {
-        let state = 42;
-        let nextState = increment(state);
+        it('is immutable', () => {
+            let state = 42;
+            let nextState = increment(state);
 
-        expect(nextState).to.equal(43);
-        expect(state).to.equal(42);
+            expect(nextState).to.equal(43);
+            expect(state).to.equal(42);
         });
+
     });
 
     describe('A List', () => {
+
         function addMovie(currentState, movie) {
             return currentState.push(movie);
         }
+
         it('is immutable', () => {
             let state = List.of('Trainspotting', '28 Days Later');
             let nextState = addMovie(state, 'Sunshine');
@@ -41,7 +45,7 @@ describe('immutability', () = > {
     describe('a tree', () => {
 
         function addMovie(currentState, movie) {
-            return currentState.update(
+            return currentState.set(
                 'movies',
                 currentState.get('movies').push(movie)
             );
@@ -69,5 +73,6 @@ describe('immutability', () = > {
         });
 
     });
+
 
 });
